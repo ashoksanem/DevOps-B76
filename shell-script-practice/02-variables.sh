bfbe48a5c7889f19b9c853d9bdbd6e5f029f20b5
DevOps-B76 <ashoksanem@gmail.com>
2024-03-22 16:00:16 +0530

    variable shell script

--- shell-script-practice/02-variables.sh
+++ shell-script-practice/02-variables.sh
@@ -4,4 +4,4 @@ PERSON_2=Suresh
 echo "$PERSON_1: Hello $PERSON_2, GM"
 echo "$PERSON_2: Hello $PERSON_1, VGM"
 echo "$PERSON_1: How'z the day $PERSON_2..?"
-echo "$PERSON_2 Its a busy day $PERSON_1, how abt u"
+echo "$PERSON_2: Its a busy day $PERSON_1, how abt u"
