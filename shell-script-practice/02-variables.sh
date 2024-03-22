7ba7eef36c2489bc9a520b86cc0ea5f24b5e552f
DevOps-B76 <ashoksanem@gmail.com>
2024-03-22 15:21:02 +0530

    variable shell script

---
+++ shell-script-practice/02-variables.sh
@@ -0,0 +1,7 @@
+#!/bain/bash
+PERSON_1=Ramesh
+PERSON_2=Suresh
+echo "$PERSON_1: Hello $PERSON_2, GM"
+echo "$PERSON_2: Hello $PERSON_1, VGM"
+echo "$PERSON_1: How'z the day $PERSON_2..?"
+echo "$PERSON_2 Its a busy day $PERSON_1, how abt u"
