36cc4b20f2d94c9a314b083fd0880e6f08f2a5d4
DevOps-B76 <ashoksanem@gmail.com>
2024-03-25 17:42:22 +0530

    forth, variable_example commit

--- shell-script-practice/04-variables.sh
+++ shell-script-practice/04-variables.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+PERSON_1=$1
+PERSON_2=$2
+echo "$PERSON_1: Hello $PERSON_2, GM"
+echo "$PERSON_2: Hello $PERSON_1, VGM"
+echo "$PERSON_1: How'z the day $PERSON_2..?"
+echo "$PERSON_2: Its a busy day $PERSON_1, how abt u"
